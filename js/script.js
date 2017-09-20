bbf0cf1447606267c3ccf1292bf3e8face40c2b7
Vitalii Khymynets <vtlkand@gmail.com>
2017-09-20 18:58:22 +0100

    Finish nav bar

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#bars-btn').click(function() {
+        $('#bars-btn').toggleClass('bars-cross');
+        $('#mobile-nav').toggleClass('show-m-nav');
+    });
+});
