348861b9e7abc3ac467a99b5957a5790c49ab963
Vitalii Khymynets <vtlkand@gmail.com>
2017-09-21 21:00:54 +0100

    Add scroll to an anchor effect

--- js/script.js
+++ js/script.js
@@ -1,8 +1,38 @@
 $(document).ready(function() {
+    //navbar toggle
     $('#bars-btn').click(function() {
         $('#bars-btn').toggleClass('bars-cross');
         $('#mobile-nav').toggleClass('show-m-nav');
     });
+
+    //scroll to the anchor
+    $.fn.scrollView = function(pos=$(this).offset().top) {
+        return this.each(function() {
+            $('html, body').animate({
+                scrollTop: pos
+            }, 600);
+        });
+    };
+
+    function navsClick(nav) {
+        $(nav).children().each(function() {
+            $(this).children().on('click', function(event) {
+                if (this.hash !== '') {
+                    event.preventDefault();
+                    var hash = this.hash;
+                    console.log(this);
+                    $(hash).scrollView();
+                }
+            })
+        });
+    }
+
+    navsClick('#desktop-nav');
+    navsClick('#mobile-nav');
+    $('#nav-title').click(function() {
+        $('#global-container').scrollView(0);
+    });
+
 });
 
 function initMap() {
