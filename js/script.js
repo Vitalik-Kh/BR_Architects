caa84a23d80871f1a854f7b1e242a5133043c654
Vitalii Khymynets <vtlkand@gmail.com>
2017-09-21 16:27:28 +0100

    Finish main sections

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ $(document).ready(function() {
         $('#mobile-nav').toggleClass('show-m-nav');
     });
 });
+
+function initMap() {
+    var leeds = {lat: 53.801225, lng: -1.549123};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: leeds
+    });
+    var marker = new google.maps.Marker ({
+        position: leeds,
+        map: map,
+    });
+
+}
